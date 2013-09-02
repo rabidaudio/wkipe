6937aa92d1f9474b7a876ef091fcf47cad22fdfc
Charles Julian Knight <charles@rabidaudio.com>
2013-09-01 21:02:58 -0400

    Truely SEO AJAX friendly sub pages now use #!faq, and mod_rewrite sends _escaped_fragment_ on to getpage.php, which prints the whole page content. https://developers.google.com/webmasters/ajax-crawling/docs/getting-started

--- wkipe-dir/php/getpage.php
+++ wkipe-dir/php/getpage.php
@@ -1,5 +1,35 @@
 <?php
 include('functions.inc.php');
+
+
+//for ajax friendly web crawlers, like so:
+//	https://developers.google.com/webmasters/ajax-crawling/docs/getting-started
+if (preg_match("/^_escaped_fragment_\=/", $_SERVER['QUERY_STRING'])){
+	//echo $_SERVER['QUERY_STRING']."<-->";
+	$page = preg_replace("/^_escaped_fragment_\=/", "", $_SERVER['QUERY_STRING']);
+	$index = file_get_contents('../../index.html');
+	$lang = handle_lang();
+	//echo $page;
+	$file_requested ='../pages/'.$page.".";
+	if (!file_exists($file_requested.$lang)){
+		$file_requested=$file_requested."en";
+	}else{
+		$file_requested=$file_requested.$lang;
+	}
+	$file = file_get_contents($file_requested);
+	//echo $file;
+	die(str_replace('<div id="inner_page"></div>', '<div id="inner_page">'.$file.'</div>', $index));
+}
+
+function print_file($page, $lang){
+	$file='../pages/'.$page.".";
+	if (!file_exists($file.$lang)){
+		@readfile($file."en");
+	}else{
+		@readfile($file.$lang);
+	}
+}
+
 if (!$_GET['page']){
 	die();
 }
@@ -9,10 +39,5 @@ if (!$_GET['lang']){
 }else{
 	$lang=$_GET['lang'];
 }
-$file='../pages/'.$page.".";
-if (!file_exists($file.$lang)){
-	@readfile($file."en");
-}else{
-	@readfile($file.$lang);
-}
+print_file($page, $lang);
 ?>
